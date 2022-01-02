983267eae81bdb7dca14890fc1bec952a9fdff64
choiyounyeong <young0770@naver.com>
2022-01-02 18:28:08 +0900

    Created service and connected to storefront API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
@@ -18,13 +18,10 @@ import { HeaderComponent } from './layout/header/header.component';
     ProductItemDetailComponent,
     ConfirmationComponent,
     CartComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpService } from 'src/app/http.service';
+import { Product } from 'src/app/models/product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private httpService: HttpService) {
+    this.products = [];
   }
 
+  ngOnInit(): void {
+    this.httpService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
