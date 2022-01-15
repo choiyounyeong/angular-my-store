88176fee4ee3438c2e40d159da716ee6ae27344d
choiyounyeong <young0770@naver.com>
2022-01-15 20:39:03 +0900

    Used ngModelChange in cart component to show error message when the value of name, address, credit card are invalid

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -10,6 +10,10 @@ import { PurchaseInfo } from 'src/app/models/customer-info';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  nameInvalidMessage: string = '';
+  addressInvalidMessage: string = '';
+  cardInvalidMessage: string = '';
+
   purchaseInfo: PurchaseInfo = {
     userName: '',
     address: '',
@@ -54,5 +58,35 @@ export class CartComponent implements OnInit {
       totalPrice: 0,
     };
     this.route.navigateByUrl('confirm');
+  }
+
+  validateName(): void {
+    if (!this.purchaseInfo.userName) {
+      this.nameInvalidMessage = 'Name is required';
+    } else if (this.purchaseInfo.userName.length < 6) {
+      this.nameInvalidMessage = 'Minimum length is 6';
+    } else {
+      this.nameInvalidMessage = '';
+    }
+  }
+
+  validateAddress(): void {
+    if (!this.purchaseInfo.address) {
+      this.addressInvalidMessage = 'Address is required';
+    } else if (this.purchaseInfo.address.length < 10) {
+      this.addressInvalidMessage = 'Minimum length is 10';
+    } else {
+      this.addressInvalidMessage = '';
+    }
+  }
+
+  validateCard(): void {
+    if (!this.purchaseInfo.creditCard) {
+      this.cardInvalidMessage = 'Credit card number is required';
+    } else if (this.purchaseInfo.creditCard.length < 16) {
+      this.cardInvalidMessage = 'Minimum length is 16';
+    } else {
+      this.cardInvalidMessage = '';
+    }
   }
 }
